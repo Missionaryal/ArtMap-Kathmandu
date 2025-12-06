e71817a993dec904a566f40db49f6b0f40bb6d27
Missionaryal <missionsthaaryal@gmail.com>
2025-12-06 14:36:26 +0545

    Setup React Frontend with Tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
